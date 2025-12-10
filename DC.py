391fd0f92c87bc097f5708076b4a84ae3a522f57
Acierrr <queenzyivanat@gmail.com>
2025-12-10 20:21:29 +0700

    Create DC.py

---
+++ DC.py
@@ -0,0 +1,26 @@
+meme_dict = {
+            "CRINGE": "Sesuatu yang sangat aneh atau memalukan",
+            "LOL": "Tanggapan umum terhadap sesuatu yang lucu",
+            }
+
+
+while True:
+    print ("apa yang mau kamu lakukan ?")
+    print( "1. menambah kata")
+    print ("2. mencari kata")
+    print ("3. tampilkan semua kata")
+    jawaban = int (input ())
+    if jawaban == 1:
+        list_baru = input  ("masukkan kata yang ingin ditambahkan :")
+        meme_dict[list_baru]=input("masukkan artinya:")
+    elif jawaban == 2:
+        kata = input("ketik kata yang ingin kamu cari")
+        if kata in meme_dict.keys():
+            print(meme_dict[kata])
+        else :
+            print ("kata yang kamu cari tidak ada")
+    elif jawaban == 3 :
+        print (meme_dict)
+    else :
+        print ("jawabanmu error")
+    
